79a96936ebfce4a8e51aad4ba2ba0e473fd44d1d
eduardo239 <eucrieiumaconta@gmail.com>
2021-07-21 10:44:56 -0300

    index.html movido para root

--- build/js/index.js
+++ build/js/index.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 var btnBackToTop = document.getElementById('btnBackToTop');
 
 var topFunction = function topFunction() {
@@ -8,3 +14,20 @@ var topFunction = function topFunction() {
 };
 
 btnBackToTop.addEventListener('click', topFunction);
+
+var Test = /*#__PURE__*/function () {
+  function Test(name) {
+    _classCallCheck(this, Test);
+
+    this.name = name;
+  }
+
+  _createClass(Test, [{
+    key: "logger",
+    value: function logger() {
+      console.log('Hello', this.name);
+    }
+  }]);
+
+  return Test;
+}();
